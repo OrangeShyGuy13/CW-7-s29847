e27054fe4dbd4a34168c8a631e3e4b4c497f9a45
OrangeShyGuy13 <jjodlowskidev@gmail.com>
2025-05-08 23:36:55 +0200

    add missing program.cs and .csproj

---
+++ program.cs
@@ -0,0 +1,12 @@
+using System.Data.SqlClient;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+app.MapControllers();
+app.Run();
